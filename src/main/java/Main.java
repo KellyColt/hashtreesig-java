4944db9c78980881bc9f3f6f21a153d44394f6d1
F. Krause <frie.krause@gmail.com>
2021-10-14 14:56:50 +0200

    javafx setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import javax.swing.*;
+import java.io.IOException;
+import java.util.Objects;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("scene.fxml")));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("main.css")).toExternalForm());
+
+        primaryStage.setTitle("JavaFX and Gradle");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
+    @Test
+    void runs() {
+        Main testClass = new Main();
+        assertNotNull(testClass);
+    }
 }
