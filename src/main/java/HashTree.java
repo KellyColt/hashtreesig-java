236c90bf57f663370ab07f4fb4485e5502b397df
F. Krause <frie.krause@gmail.com>
2021-10-24 23:58:00 +0200

    Algorithm for building and signing the hashtree implemented, untested

--- src/main/java/HashTree.java
+++ src/main/java/HashTree.java
@@ -1,26 +1,107 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.Signature;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.security.interfaces.ECPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.*;
 
 public class HashTree {
     public boolean closed;
-    public ArrayList<byte[]> leaves;
-    public ArrayList<byte[]> nodes;
-    public final byte[] ecdsa_sig;
-    private Certificate cert;
+    private ArrayList<byte[]> leaves;
+    private ArrayList<ArrayList<byte[]>> nodes;
+    public byte[] ecdsa_sig;
+    private ECPrivateKey key;
+    private MessageDigest digest;
+    private SecureRandom srand;
 
-    public HashTree(File certFile) throws CertificateException, FileNotFoundException {
+    public HashTree(File keyFile) throws IOException {
         this.closed = false;
         this.leaves = new ArrayList<>();
 
         this.nodes = new ArrayList<>();
-        this.ecdsa_sig = new byte[32];
+        this.nodes.add(leaves);
 
-        this.cert = CertificateFactory.getInstance("X.509").generateCertificate(new FileInputStream(certFile));
+        this.srand = new SecureRandom();
+
+        // this.cert = CertificateFactory.getInstance("X.509").generateCertificate(new FileInputStream(keyFile));
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(Files.readAllBytes(keyFile.toPath()));
+        try {
+            this.key =(ECPrivateKey) KeyFactory.getInstance("EC").generatePrivate(spec);
+            this.digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Instantiation Error: Algorithm");
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            System.err.println("your spec is invalid you absolute fucking idiot");
+            e.printStackTrace();
+        }
+    }
+
+    public void add(byte[] msg) throws IllegalStateException{
+        if (this.closed) throw new IllegalStateException("this hashtree is closed");
+        leaves.add(digest.digest(msg));
+        byte[] token = new byte[32];
+        srand.nextBytes(token);
+        leaves.add(token);
+    }
+
+    public void sign() throws IllegalStateException{
+        if (this.closed) throw new IllegalStateException("this tree is already closed");
+
+        int power2 = (int) Math.ceil(Math.log(this.leaves.size()) / Math.log(2));
+        int missing = (int) Math.pow(2,  power2) - this.leaves.size();
+        for (int i = 0; i < missing; i++) {
+            byte[] token = new byte[32];
+            srand.nextBytes(token);
+            leaves.add(token);
+        }
+        ArrayList<byte[]> last = nodes.get(nodes.size() - 1);
+        if (!last.equals(leaves)) throw new IllegalStateException("Something went real wrong babes");
+        else if(last.isEmpty()) throw new IllegalStateException("no messages to sign");
+        else if(last.size() > 1) {
+            do {
+                ArrayList<byte[]> neu = new ArrayList<>();
+                for (int i = 0; i < last.size(); i += 2) {
+                    try {
+                        ByteArrayOutputStream outp = new ByteArrayOutputStream();
+                        outp.write(last.get(i));
+                        outp.write(last.get(i + 1));
+                        neu.add(digest.digest(outp.toByteArray()));
+                    } catch (IOException e) {
+                        System.err.println("concat error");
+                        e.printStackTrace();
+                        return;
+                    }
+                }
+                nodes.add(neu);
+                last = neu;
+            } while (last.size() != 1);
+        }
+        try {
+            Signature signature = Signature.getInstance("SHA256withECDSA");
+            signature.initSign(key, srand);
+            signature.update(last.get(0));
+            this.ecdsa_sig = signature.sign();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Signature Algorithm Instantiation Error");
+            e.printStackTrace();
+            return;
+        } catch (InvalidKeyException e) {
+            System.err.println("Invalid Key Exception at signature initialisation");
+            e.printStackTrace();
+            return;
+        } catch (SignatureException e) {
+            System.err.println("Error in signing process");
+            e.printStackTrace();
+            return;
+        }
+
+        this.closed = true;
     }
 }
--- src/main/java/Signature.java
+++ src/main/java/Signature.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.cert.CertificateException;
 
 public class Signature {
@@ -8,22 +10,24 @@ public class Signature {
         System.out.println("Hello World");
 
         File certFile = new File("pki/signer_cert.der");
+        File keyFile = new File("pki/signing_key.der");
         HashTree merkle;
         try {
-            merkle = new HashTree(certFile);
-        } catch (CertificateException e) {
-            System.err.println("Invalid Certificate");
-            e.printStackTrace();
-            return;
-        } catch (FileNotFoundException e) {
-            System.err.println("Certificate File not Found");
+            merkle = new HashTree(keyFile);
+        } catch (IOException e) {
+            System.err.println("Error reading Keyfile");
             e.printStackTrace();
             return;
         }
         System.out.printf("Ich erzeuge ad-hoc %d Nachrichten %n", test_nr);
 
         for (int i = 0; i < test_nr; i++) {
-
+            byte[] msg = ("Hallo, ich bin Nachricht Nr. " + i).getBytes(StandardCharsets.UTF_8);
+            merkle.add(msg);
+            System.out.println("Added message " + i);
         }
+
+        merkle.sign();
+        System.out.println(merkle.ecdsa_sig);
     }
 }
