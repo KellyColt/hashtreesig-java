fac5d44b42090bad436a5224d85d61994325992d
F. Krause <frie.krause@gmail.com>
2021-10-24 23:58:00 +0200

    Algorithm for building and signing the hashtree implemented, untested

--- src/main/java/Signature.java
+++ src/main/java/Signature.java
@@ -1,29 +1,32 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.security.cert.CertificateException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Signature {
-    private static int test_nr = 3;
+    private static final int test_nr = 3;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        File certFile = new File("pki/signer_cert.der");
+        // File certFile = new File("pki/signer_cert.der");
+        File keyFile = new File("pki/signing_key.der");
         HashTree merkle;
         try {
-            merkle = new HashTree(certFile);
-        } catch (CertificateException e) {
-            System.err.println("Invalid Certificate");
-            e.printStackTrace();
-            return;
-        } catch (FileNotFoundException e) {
-            System.err.println("Certificate File not Found");
+            merkle = new HashTree(keyFile);
+        } catch (IOException e) {
+            System.err.println("Error reading Keyfile");
             e.printStackTrace();
             return;
         }
         System.out.printf("Ich erzeuge ad-hoc %d Nachrichten %n", test_nr);
 
         for (int i = 0; i < test_nr; i++) {
-
+            byte[] msg = ("Hallo, ich bin Nachricht Nr. " + i).getBytes(StandardCharsets.UTF_8);
+            merkle.add(msg);
+            System.out.println("Added message " + i);
         }
+
+        merkle.sign();
+        System.out.println(merkle.bytesToHex(merkle.ecdsa_sig));
     }
 }
